1b7774e5974e6589d03ebfb3bb321a905209fe2f
greyhound <1174062640@qq.com>
2022-06-28 21:51:42 +0800

    update

---
+++ tools/.ycm_extra_conf.py
@@ -0,0 +1,115 @@
+import platform
+import os.path as p
+import subprocess
+import ycm_core
+import os
+
+from distutils.sysconfig import get_python_inc
+from pathlib import Path
+
+flags = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-x',
+    'c++',
+    ]
+
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
+HEADER_EXTENSIONS = [ '.h', '.hxx', '.hpp', '.hh' ]
+Deps = set()
+
+def GetDataBase():
+    compilation_database_folder = Path(DIR_OF_THIS_SCRIPT + "/build")
+
+    database = None
+    if compilation_database_folder.joinpath("compile_commands.json").exists():
+        database = ycm_core.CompilationDatabase( compilation_database_folder.as_posix() )
+
+    return database
+
+def IsHeaderFile( filename ):
+  extension = p.splitext( filename )[ 1 ]
+  return extension in HEADER_EXTENSIONS
+
+def FindCorrespondingSourceFile( filename ):
+  if IsHeaderFile( filename ):
+    basename = p.splitext( filename )[ 0 ]
+    for extension in SOURCE_EXTENSIONS:
+      replacement_file = basename + extension
+      if p.exists( replacement_file ):
+        return replacement_file
+
+  return filename
+
+def debug(info):
+    abspath = Path(DIR_OF_THIS_SCRIPT).joinpath("ycm.log")
+
+    with open(abspath, "a") as f:
+      f.write(str(info) + "\n")
+
+def GetDependenciesInc(deps):
+  libdir = Path(DIR_OF_THIS_SCRIPT).parent.joinpath("lib/win64_vc14")
+  addition_include = [DIR_OF_THIS_SCRIPT,
+      "glib/include",
+      "glib/include/gio",
+      "glib/include/gio-win32-2.0",
+      "glib/include/glib",
+      "glib/include/glib-2.0",
+      "glib/include/gobject",
+      "cglm/include",
+      "glad/include",
+      "glfw3/include",
+      "Unity/include",
+      "F:/Program Files/VulkanSDK/1.3.204.1/Include"
+  ]
+
+  for h in addition_include:
+    inc = libdir.joinpath(h)
+    deps.add("-I" + inc.as_posix())
+
+def GetRecursiveHeaderDir(deps):
+    for dp, dns, fns in os.walk(DIR_OF_THIS_SCRIPT):
+        for f in fns:
+            if f.endswith(".h"):
+                deps.add("-I" + Path(dp).joinpath(f).parent.as_posix())
+
+def PathToPythonUsedDuringBuild():
+  try:
+    filepath = p.join( DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING' )
+
+    with open( filepath ) as f:
+      return f.read().strip()
+
+  except ( IOError, OSError ):
+    return None
+
+def Settings( **kwargs ):
+  language = kwargs[ 'language' ]
+
+  if language == 'cfamily':
+    filename = FindCorrespondingSourceFile(kwargs[ 'filename' ])
+
+    GetDependenciesInc(Deps)
+    GetRecursiveHeaderDir(Deps)
+
+    flags.append("-I" + Path(filename).parent.as_posix())
+    flags.extend(Deps)
+
+    return {
+        'flags': flags,
+        'include_paths_relative_to_dir': DIR_OF_THIS_SCRIPT,
+        'override_filename': filename,
+        "do_cache": True
+    }
+
+  if language == 'python':
+    return {
+        'interpreter_path': PathToPythonUsedDuringBuild()
+        }
+
+  return {}
