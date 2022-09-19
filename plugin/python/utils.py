95386fdf6b6708881725188ab21cc9efec898495
greyhound <1174062640@qq.com>
2022-09-20 03:06:31 +0800

    update

--- plugin/python/utils.py
+++ plugin/python/utils.py
@@ -2,10 +2,20 @@ import subprocess
 import sys
 import os
 import json
+import vimpy
 
+from enum import IntEnum
+
+comment_template = """
+/**
+ * ${func_name}
+ * @${param}:
+ *
+ * Returns: None
+ */
+"""
 
 vsInfo = None
-
 
 def process_cmd(cmd, cwd):
     """ Abstract subprocess """
@@ -26,6 +36,7 @@ def process_cmd(cmd, cwd):
 
     return stdout
 
+
 def get_vs_info():
     global vsInfo
 
@@ -42,6 +53,7 @@ def get_vs_info():
     vsInfo = json.loads(out)
     return vsInfo
 
+
 def get_vs_header_path():
     vss = get_vs_info()
     vs = None
@@ -57,6 +69,7 @@ def get_vs_header_path():
         inc_path = "%s/VC/Tools/MSVC/14.16.27023/include" % (vs["installationPath"])
 
     return inc_path.replace("\\", "/")
+
 
 def get_system_header_path():
     incs = None
@@ -79,6 +92,111 @@ def get_system_header_path():
 
     return incs
 
+
 def get_system_header_str():
     hds = get_system_header_path()
     return ",".join(hds).replace(" ", "\\ ")
+
+
+class FileType(IntEnum):
+    LANG_C      = 1
+    LANG_PYTHON = 2
+    LANG_CSHARP = 3
+
+class CommentParser:
+    def __init__(self):
+        self.offset = 0
+        self.line = None
+
+    def func_comment(self):
+        pass
+
+    def lexer_next_c(self, reverse = False):
+        if reverse:
+            self.offset -= 1
+        else:
+            self.offset += 1
+
+        if self.offset <= 0:
+            return '\0'
+
+        return self.line[self.offset]
+
+    def is_id(self, c):
+        return (c >= '0' and c <= '9') or (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z')
+
+    def seek(self, p):
+        self.offset = p
+
+    def seek_c(self, c):
+        p = self.line[self.offset:].find(c)
+        if p > -1:
+            self.offset = self.offset + p
+
+        return p
+
+    def lexer_next(self, reverse = False):
+        iden = ""
+
+        while True:
+            c = self.lexer_next_c(reverse)
+
+            while self.is_id(c):
+                iden += c
+                c = self.lexer_next_c(reverse)
+                continue
+
+            if c == '\n' or c == '\0':
+                break
+
+            if c == '(':
+                break
+
+            if c == ' ':
+                break
+
+        if reverse:
+            niden = ""
+            for i in reversed(iden):
+                niden += i
+
+            iden = niden
+
+        return iden
+
+    def parse_c_proto(self):
+        iden = None
+
+        #       return,   name, [params]
+        proto = [None,    None,      None]
+
+        p = self.seek_c("(")
+        if p == -1:
+            return
+
+        iden = self.lexer_next(True)
+        proto[1] = iden
+
+        iden = self.lexer_next(True)
+        proto[0] = iden
+
+        self.seek(p)
+        iden = self.lexer_next()
+
+    def get_filelang(self, filename):
+        if filename.endswith(".c"):
+            return FileType.LANG_C
+        elif filename.endswith(".py"):
+            return FileType.LANG_PYTHON
+        elif filename.endswith(".cs"):
+            return FileType.LANG_CSHARP
+
+    def get_comment(self):
+        lang = self.get_filelang(vimpy.vim_fullname())
+
+        self.line = vimpy.vim_get_line()
+        if lang == FileType.LANG_C:
+            proto = self.parse_c_proto()
+
+g_comment = CommentParser()
+g_comment.get_comment()
--- plugin/python/vimpy.py
+++ plugin/python/vimpy.py
@@ -1,7 +1,8 @@
-import vim
+# import vim
 
 
 def vim_fullname():
+    return "E:/Codes/REPOSITORY/vim/plugins/vimext/plugin/python/vimpy.c"
     return vim.eval("expand(\"%:p\")")
 
 def vim_ctags_bin():
@@ -9,3 +10,8 @@ def vim_ctags_bin():
 
 def vim_has(name):
     return int(vim.eval('has("%s")' % (name)))
+
+def vim_get_line():
+    return "void stati_get_func(void* a,void *b);"
+    line = vim.eval("getline(\".\")")
+    return line
